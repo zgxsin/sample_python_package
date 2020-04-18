d66a82c2a6296fb2b92879c11fb6fe6c8a7a660b
Guoxiang Zhou <gzhou@anybotics.com>
2020-04-18 19:08:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,4 @@
 #!/usr/bin/env python
+
+# When you finish your project, create your requirements.txt using "pip freeze > requirements.txt" in your project root.
 raise NotImplementedError("Setup not implemented yet.")
