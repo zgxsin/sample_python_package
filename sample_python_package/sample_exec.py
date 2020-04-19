df4e1e5639f0a505909f5423cc157d1a89951487
Guoxiang Zhou <zgxsin@qq.com>
2020-04-19 10:13:08 +0200

    Add a link

--- sample_python_package/sample_exec.py
+++ sample_python_package/sample_exec.py
@@ -5,6 +5,7 @@ import os
 def main():
     """Entry point for the application script"""
     # Use absolute path for files, otherwise you can not find it after you install the package.
+    # How to add parsers: https://docs.python.org/3/library/argparse.html#required
     path = os.path.join(os.path.dirname(__file__), 'fixtures/calories.csv')
     for row in dataset(path):
         print(row[0])
